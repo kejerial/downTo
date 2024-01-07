6453529d30bb76e660d81108f6cc922c892e1b01
Kevin Jeon <kevinj1115@gmail.com>
2024-01-06 18:39:26 -0800

    Initial Commit

---
+++ downTo/downToApp.swift
@@ -0,0 +1,17 @@
+//
+//  downToApp.swift
+//  downTo
+//
+//  Created by Kevin Jeon on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct downToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
